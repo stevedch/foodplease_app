d04def3812403de1a6c960582065fd8177da5f1c
Steven Delgado Chacón <stevedch@users.noreply.github.com>
2025-05-12 19:29:38 -0400

    refactor(app): restructure project folders under 'presentation' layer
    - Moved all `screens` and `widgets` from `lib/` into `lib/presentation/` to align with feature-layered architecture.
    - Updated `main.dart` to reference `LoginScreen` from the new `presentation/screens` path.
    - Renamed files without modifying content to maintain existing functionality while improving code organization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:foodplease_app/presentation/screens/login_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'screens/login_screen.dart';
 
 void main() {
   runApp(const FoodPleaseApp());
